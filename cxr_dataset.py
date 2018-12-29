3d7b8390734b176ab72bfe8a801938fb1e681fcf
vija0014 <46249468+vija0014@users.noreply.github.com>
2018-12-29 22:14:18 +0100

    Update cxr_dataset.py

--- cxr_dataset.py
+++ cxr_dataset.py
@@ -18,11 +18,11 @@ class CXRDataset(Dataset):
 
         self.transform = transform
         self.path_to_images = path_to_images
-        self.df = pd.read_csv("Documents/reproduce-chexnet-master/nih_labels.csv")
+        self.df = pd.read_csv("nih_labels.csv")
         self.df = self.df[self.df['fold'] == fold]
 
         if(starter_images):
-            starter_images = pd.read_csv("Documents/reproduce-chexnet-master/nih_labels.csv")
+            starter_images = pd.read_csv("nih_labels.csv")
             self.df=pd.merge(left=self.df,right=starter_images, how="inner",on="Image Index")
             
         # can limit to sample, useful for testing

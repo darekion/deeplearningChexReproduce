206009c19ffdf1ea89d5fe5376270b9e1044dc6f
vija0014 <46249468+vija0014@users.noreply.github.com>
2018-12-29 21:57:21 +0100

    Update train_concat.py

--- train_concat.py
+++ train_concat.py
@@ -1,23 +1,23 @@
 import model_concat as M
 import sys
 
-method = sys.argv[1]
-thresh = float(sys.argv[2])
-mean = bool(int(sys.argv[3]))
+method = 'Complex'
+thresh = 0.5
+mean = False
 heatmap_methods = [method, thresh,mean]
 
-local_base_path = '/zhome/45/0/97860/Documents/reproduce-chexnet-master/results_local2/state_dict'
-global_state_dict = '/zhome/45/0/97860/Documents/reproduce-chexnet-master/Results/state_dict'
+local_base_path = 'results_local/state_dict'
+global_state_dict = 'results_global/state_dict'
 local_state_dict = local_base_path + '_t' + str(thresh) + '_m=' + str(mean)
 
-fusion_base_path = '/zhome/45/0/97860/Documents/reproduce-chexnet-master/results_concat/'
+fusion_base_path = 'results_fusion/'
 state_dict_path = fusion_base_path + 'state_dict_t' + str(thresh) + '_m=' + str(mean)
 model_path = fusion_base_path + 'checkpoint_t' + str(thresh) + '_m=' + str(mean)
 aucs_path = fusion_base_path + 'aucs_t' + str(thresh) + '_m=' + str(mean) + '.csv'
 preds_path = fusion_base_path + 'preds_t' + str(thresh) + '_m=' + str(mean) + '.csv'
 paths = [state_dict_path, model_path, aucs_path, preds_path]
 
-path_to_images = '/work3/s144137/DL/extracted_images/images'
+path_to_images = '/work3/s144137/DL/extracted_images/images' # Set path to images
 
 LR = 0.01
 weight_decay = 1e-4
